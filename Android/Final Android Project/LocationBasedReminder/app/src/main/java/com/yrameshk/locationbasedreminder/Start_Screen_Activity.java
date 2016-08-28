b2b53c50951b5f7f3fa91d34607b278b64f9eee9
yrameshk26 <yrameshk26@gmail.com>
2016-08-27 23:41:54 -0400

    last

--- Android/Final Android Project/LocationBasedReminder/app/src/main/java/com/yrameshk/locationbasedreminder/Start_Screen_Activity.java
+++ Android/Final Android Project/LocationBasedReminder/app/src/main/java/com/yrameshk/locationbasedreminder/Start_Screen_Activity.java
@@ -1,19 +1,39 @@
 package com.yrameshk.locationbasedreminder;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Start_Screen_Activity extends AppCompatActivity {
+
+    final private int REQUEST_CODE_SOME_FEATURES_PERMISSIONS = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
         getSupportActionBar().hide();
+        PermissionChecker();
         if( getIntent().getBooleanExtra("Exit me", false)){
             finish();
+        }
+    }
+
+    private void PermissionChecker() {
+        int hasLocationPermission = checkSelfPermission( Manifest.permission.ACCESS_FINE_LOCATION );
+        List<String> permissions = new ArrayList<String>();
+        if( hasLocationPermission != PackageManager.PERMISSION_GRANTED ) {
+            permissions.add( Manifest.permission.ACCESS_FINE_LOCATION );
+        }
+
+        if( !permissions.isEmpty() ) {
+            requestPermissions( permissions.toArray( new String[permissions.size()] ), REQUEST_CODE_SOME_FEATURES_PERMISSIONS );
         }
     }
 
